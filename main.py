c08dff6631ab086c25fb1fc3a40c3078c10c2fdb
onkanat <47117474+onkanat@users.noreply.github.com>
2023-10-20 19:44:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,14 +21,20 @@ def pencere():
 
     layout=[
         [sg.Menu(menu_definition=menu_def)],
+        [sg.Push(), 
+         sg.Button(' Ana Pencere ', key='-ANA-',
+                                            image_filename='/Users/hakankilicaslan/GitHub/Machining_Formulas/T150/icons8-arrow-quill-3/icons8-arrow-100.png'),
+                                              sg.Push()],
+        [
         [sg.Button(' Ana Pencere ', key='-ANA-',
                                             image_filename='/Users/hakankilicaslan/GitHub/Machining_Formulas/T150/icons8-arrow-quill-3/icons8-arrow-100.png'),
         sg.Button(' Kesim Listesi Giriş', key='-GIRIS-',
-                                            image_filename='/Users/hakankilicaslan/GitHub/Machining_Formulas/T150/icons8-arrow-quill-3/icons8-arrow-100.png'),
+                                            image_filename='/Users/hakankilicaslan/GitHub/Machining_Formulas/T150/icons8-drawing-compass-quill/icons8-drawing-compass-100.png'),
         sg.Button(' Kesim Listesi Giriş', key='-GIRIS-',
-                                            image_filename='/Users/hakankilicaslan/GitHub/Machining_Formulas/T150/icons8-arrow-quill-3/icons8-arrow-100.png'),
+                                            image_filename='/Users/hakankilicaslan/GitHub/Machining_Formulas/T150/icons8-caliper-quill/icons8-caliper-100.png'),
         sg.Button(' Kesim Listesi Giriş', key='-GIRIS-',
-                                            image_filename='/Users/hakankilicaslan/GitHub/Machining_Formulas/T150/icons8-arrow-quill-3/icons8-arrow-100.png')]
+                                            image_filename='/Users/hakankilicaslan/GitHub/Machining_Formulas/T150/icons8-saw-blade-quill-1/icons8-saw-blade-100.png')]
+        ]
 ]
 
     window = sg.Window('T150 Giriş Ekranı',layout=layout)
